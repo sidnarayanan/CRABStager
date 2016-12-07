d57a891ce9defc21080edbdd27b638029c49098b
Siddharth Narayanan <sidn@mit.edu>
2016-12-07 15:56:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+A simple tool to stage out output from CRAB3 that is stuck in 'transferring' state. Two environments are needed: one to catalog CRAB jobs (set up CMSSW and CRAB), and another to do gfal-copy (CMSSW interfers with the gfal libraries).
+
+In the CMSSW environment:
+
+```
+# ./collectPFNs.py --subdir /path/to/CRAB/submission/directory/ --outdir /path/to/task/output/
+./collectPFNs.py --subdir /afs/cern.ch/user/s/snarayan/private/CMSSW_8_0_20/src/PandaProd/Ntupler/test/Submission/crab_SingleElectron_Run2016E-23Sep2016-v1/ --outdir /afs/cern.ch/user/s/snarayan/eos/cms/store/group/phys_exotica/monotop/pandaprod/v_8022_2_snarayan/SingleElectron/SingleElectron_Run2016E-23Sep2016-v1/161206_231050/
+```
+
+This will dump to screen a bunch of `gfal-copy` commands for jobs that are in 'transferring' state and the output does not yet exist in `--outdir`. Simply run these commands in the second environment and watch your output appear! (Although gfal-copy fails a bunch). The default behavior is not to overwrite files that exist, so there should be no danger of losing data.
---
+++ collectPFNs.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import subprocess
+import re
+import json
+import argparse
+from sys import argv
+import pprint 
+from StringIO import StringIO
+import os
+
+class Job():
+  def __init__(self,jid):
+    self.jid=jid
+    self.site=None
+    self.pfn=None
+    self.lfn=None
+
+parser = argparse.ArgumentParser(description='determine which outputs are stuck transferring')
+parser.add_argument('--outdir',metavar='outdir',type=str)
+parser.add_argument('--subdir',metavar='subdir',type=str)
+args = parser.parse_args()
+
+transferring = [] # list of Jobs
+
+print 'Requesting summary'
+# determine stuck jobs
+proc_crabStatus = subprocess.Popen(['crab','status','--long',args.subdir],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out_crabStatus,err_crabStatus = proc_crabStatus.communicate()
+state=0
+for line in out_crabStatus.split('\n'):
+  if 'Most Recent Site' in line:
+    state=1
+    continue
+  if 'Summary' in line or line.strip()=='':
+    state=0
+    continue
+  if state==1:
+    ll = line.split()
+    if ll[1]=='transferring':
+      j = Job(int(ll[0]))
+      j.site=ll[2].strip()
+      if 'T1' in j.site:
+        j.site += '_Disk'
+      transferring.append(j)
+
+#pprint.pprint(transferring)
+
+# request logs for those jobs
+jidsToGet = []
+for job in transferring:
+  fpath = args.subdir+'/results/job_out.%i.0.txt'%(job.jid)
+  if not os.path.exists(fpath): # log doesn't exist
+    jidsToGet.append(job.jid)
+jobidstr = ','.join([str(x) for x in jidsToGet])
+print 'Requesting logs for %i jobs'%(len(jidsToGet))
+proc_crabLog = subprocess.Popen(['crab','getlog','--short','--dir',args.subdir,'--jobids',jobidstr],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out_crabLog,err_crabLog = proc_crabLog.communicate()
+
+print 'Parsing logs'
+# parse logs to determine lfns
+for job in transferring:
+  fpath = args.subdir+'/results/job_out.%i.0.txt'%(job.jid)
+  flog = open(fpath)
+  for line in flog:
+    if 'JOB_CMSSite' in line and job.site==None: # why?
+      sitename = re.sub('["\s]','',line.strip().split('=')[-1])
+      if 'T1' in sitename:
+        sitename += '_Disk'
+      job.site = sitename
+    if 'source_lfn' in line:
+      lfn = re.sub("[',\s]","",line.strip().split(':')[-1])
+      job.lfn = lfn
+
+print 'Converting LFNs'
+# determine the pfns
+for job in transferring:
+  url = 'https://cmsweb.cern.ch/phedex/datasvc/json/prod/lfn2pfn?lfn=%s&node=%s&protocol=srmv2'%(job.lfn,job.site)
+  try:
+    proc_phedexPFN = subprocess.Popen(['curl','-k',url],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out_phedexPFN,err_phedexPFN = proc_phedexPFN.communicate()
+    io = StringIO(out_phedexPFN)
+    payload = json.load(io)['phedex']
+    pfn = payload['mapping'][0]['pfn'] # hopefully exactly one?
+    job.pfn = pfn
+  except:
+    print url
+
+# check if file is not there, and if not, then print as a candidate for copying
+for job in transferring:
+  if job.pfn==None:
+    # something failed
+    continue
+  adddir = job.jid/1000
+  outpath = '%s/%.4i/%s'%(args.outdir,adddir,job.pfn.split('/')[-1])
+  if not os.path.exists(outpath):
+    print 'gfal-copy %s %s/%.4i/'%(job.pfn,args.outdir,adddir)
+#  else:
+#    print outpath
+
+
+
+
+
+
+
+
+
+
+
+
+
